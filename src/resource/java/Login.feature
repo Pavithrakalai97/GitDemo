2edc43839d02b60572112cc9d2d354ede37ae7eb
Training_C2A.04.30 <Training_C2A.04.30@CDC2-D-521S109.dir.svc.accenture.com>
2019-07-10 12:50:29 +0530

    Successfully Committed

---
+++ src/resource/java/Login.feature
@@ -0,0 +1,6 @@
+Feature: Login functionality
+    Test the login functionality
+Scenario: Login using valid credentials
+Given User is on the home page
+When User enter the valid credential
+Then User must be in home page and display success message
---
+++ src/test/java/Pom/Package/TestRunner.java
@@ -0,0 +1,12 @@
+package Pom.Package;
+
+import org.junit.runner.RunWith;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\Training_C2A.04.30\\Pavi\\Package\\src\\resource\\java\\Login.feature")
+public class TestRunner {
+  
+}
